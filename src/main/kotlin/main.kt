f5a19023e539140c48a37c9f50e51a104655d3ef
Shikha <shikha@acceldata.io>
2023-01-13 12:22:49 +0530

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,4 @@
+fun main(){
+    println("hello kotlin")
+    var task:task_details = task_details()
+}
---
+++ src/main/kotlin/task_details.kt
@@ -0,0 +1,12 @@
+class task_details {
+    val ID:Int = 0
+    val name:String=""
+    var description: String=""
+    var created_by: String=""
+    var created_time: String=""
+    var updated_time: String=""
+    var assignee: String=""
+    var status: String=""
+    var severity: String=""
+}
+
